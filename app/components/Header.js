e25e67efeb0c280dbe46a833eceb79220c2011e7
clarence00 <clarencenatividad12@gmail.com>
2025-02-08 23:22:55 +0800

    [UPDATE]: Refactored client side.

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,44 +1,18 @@
 "use client";
+
 import React from "react";
 import { usePathname } from "next/navigation";
-import { House, Search } from "lucide-react";
-import Link from "next/link";
+import HeaderUI from "../ui/HeaderUI";
 
 const Header = () => {
   const pathname = usePathname();
-
   const links = ["/", "/search"];
+
   return (
-    <>
-      <div className="bg-bgMain navbar min-h-3 p-0 flex">
-        <Link
-          href={links[0]}
-          className={`btn w-[15%] border-none pl-6 m-2 ml-1 justify-start hover:bg-bgSecondary/[0.9] ${
-            pathname === links[0]
-              ? "bg-bgSecondary text-fgPrimary"
-              : "bg-bgMain text-fgSecondary"
-          }`}>
-          <House
-            color={pathname === links[0] ? "#E0E0E0" : "#898989"}
-            size={20}
-          />
-          Home
-        </Link>
-        <Link
-          href={links[1]}
-          className={`btn w-[15%] border-none pl-6 m-2 ml-1 justify-start hover:bg-bgSecondary/[0.9] ${
-            pathname === links[1]
-              ? "bg-bgSecondary text-fgPrimary"
-              : "bg-bgMain text-fgSecondary"
-          }`}>
-          <Search
-            color={pathname === links[1] ? "#E0E0E0" : "#898989"}
-            size={20}
-          />
-          Search
-        </Link>
-      </div>
-    </>
+    <HeaderUI
+      pathname={pathname}
+      links={links}
+    />
   );
 };
 
