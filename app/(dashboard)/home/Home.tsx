27e2eb91b4aee4b74e1498b54a35cced0c76610f
clarence00 <clarencenatividad12@gmail.com>
2025-06-05 16:49:51 +0800

    refactored view display

--- app/(dashboard)/home/Home.tsx
+++ app/(dashboard)/home/Home.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useState } from "react";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Dot, Play, Plus, PlusCircle } from "lucide-react";
 import { useAudio } from "../../context/AudioContext";
 import useFetchAudioFiles from "../../hooks/useFetchAudioFiles";
 import { List, LayoutGrid, AlignJustify, Heart } from "lucide-react";
 import AlbumCard from "../../components/AlbumCard";
+import ListViewDisplay from "../../components/ListViewDisplay";
+import CompactViewDisplay from "../../components/CompactViewDisplay";
 
 const Home = () => {
   const { audioFiles, isLoading } = useFetchAudioFiles();
@@ -25,126 +27,122 @@ const Home = () => {
 
   if (selectedAlbum) {
     const files = albums[selectedAlbum];
+
+    const durationToSeconds = (duration: string) => {
+      const [min, sec] = duration.split(":").map(Number);
+      return min * 60 + sec;
+    };
+    const formatDuration = (seconds: number) => {
+      const h = Math.floor(seconds / 3600);
+      const m = Math.floor((seconds % 3600) / 60);
+      const s = Math.floor(seconds % 60);
+      return h > 0
+        ? `${h}:${m.toString().padStart(2, "0")}:${s
+            .toString()
+            .padStart(2, "0")}`
+        : `${m}:${s.toString().padStart(2, "0")}`;
+    };
+
+    const totalDurationSeconds = files.reduce((acc, file) => {
+      return acc + durationToSeconds(file.duration || "0:00");
+    }, 0);
+
+    const totalDurationFormatted = formatDuration(totalDurationSeconds);
     return (
       <>
-        <div className="flex justify-between">
-          <button
-            className="mb-4 flex items-center text-fgSecondary w-full hover:text-fgPrimary"
-            onClick={() => setSelectedAlbum(null)}>
-            <ArrowLeft className="mr-2 size-5" />
-            Back to Albums
-          </button>
-          <div className="flex justify-end gap-4 w-full mb-4 pr-2">
-            <div
-              className={`hover:bg-fgTertiary p-1 rounded-md cursor-pointer ${
-                selectedAlbumViewMode === "list"
-                  ? "bg-bgSecondary text-fgPrimary pointer-events-none"
-                  : "text-fgSecondary"
-              }`}
-              onClick={() => setSelectedAlbumViewMode("list")}>
-              <List className="size-5" />
+        <div className="flex gap-16">
+          <div className="w-full flex flex-col overflow-y-auto">
+            <button
+              className="mb-2.5 flex items-center text-fgSecondary w-full hover:text-fgPrimary"
+              onClick={() => setSelectedAlbum(null)}>
+              <ArrowLeft className="mr-2 size-5" />
+              Back to Albums
+            </button>
+            <div className="flex justify-between">
+              <h2 className="text-2xl ml-1 font-bold w-full text-fgPrimary">
+                {selectedAlbum}
+              </h2>
+              <div className="flex justify-end gap-4 w-full pr-2">
+                <div
+                  className={`hover:bg-fgTertiary p-1 rounded-md cursor-pointer ${
+                    selectedAlbumViewMode === "list"
+                      ? "bg-bgSecondary text-fgPrimary pointer-events-none"
+                      : "text-fgSecondary"
+                  }`}
+                  onClick={() => setSelectedAlbumViewMode("list")}>
+                  <List className="size-5" />
+                </div>
+                <div
+                  className={`hover:bg-fgTertiary p-1 rounded-md cursor-pointer ${
+                    selectedAlbumViewMode === "compact"
+                      ? "bg-bgSecondary text-fgPrimary pointer-events-none"
+                      : "text-fgSecondary"
+                  }`}
+                  onClick={() => setSelectedAlbumViewMode("compact")}>
+                  <AlignJustify className="size-5" />
+                </div>
+              </div>
             </div>
-            <div
-              className={`hover:bg-fgTertiary p-1 rounded-md cursor-pointer ${
-                selectedAlbumViewMode === "compact"
-                  ? "bg-bgSecondary text-fgPrimary pointer-events-none"
-                  : "text-fgSecondary"
-              }`}
-              onClick={() => setSelectedAlbumViewMode("compact")}>
-              <AlignJustify className="size-5" />
+            <div className="flex gap-1 items-center ml-1 text-sm text-fgSecondary mb-2">
+              <span>{files[0].artist}</span>
+              <Dot className="size-4" />
+              <span>{files.length} songs</span>
+              <Dot className="size-4" />
+              <span>{totalDurationFormatted}</span>
             </div>
-          </div>
-        </div>
-        <h2 className="text-2xl ml-1 font-bold mb-2 text-fgPrimary">
-          {selectedAlbum}
-        </h2>
-        <div className="flex flex-col gap-2">
-          <div className="px-2 grid grid-cols-12 font-semibold mt-4 text-fgSecondary">
-            <span
-              className={`${
-                selectedAlbumViewMode === "list" ? "col-span-5" : "col-span-4"
-              }`}>
-              Title
-            </span>
-            {selectedAlbumViewMode === "compact" && (
-              <span className="col-span-3">Artist</span>
-            )}
-            <span
-              className={`${
-                selectedAlbumViewMode === "list" ? "col-span-4" : "col-span-3"
-              }`}>
-              Album
-            </span>
-            <span>Duration</span>
-            <span></span>
-          </div>
-          <div className="h-0.5 bg-fgTertiary w-full rounded-full mb-2.5" />
-          {files.map((file) =>
-            selectedAlbumViewMode === "list" ? (
-              <div
-                key={file.name}
-                className="p-2 grid grid-cols-12 items-center w-full bg-bgSecondary cursor-pointer hover:bg-fgTertiary rounded-lg"
-                onClick={() => playAudio(file)}>
-                <div className="flex gap-4 items-center col-span-5">
-                  <img
-                    src={file.albumArt}
-                    alt={file.name}
-                    className="w-12 h-12 rounded-lg"
+            <div className="flex flex-col gap-2">
+              <div className="px-2 grid grid-cols-12 font-semibold mt-4 text-fgSecondary">
+                <span
+                  className={`${
+                    selectedAlbumViewMode === "list"
+                      ? "col-span-6"
+                      : "col-span-6"
+                  }`}>
+                  Title
+                </span>
+                <span>Duration</span>
+                <span></span>
+              </div>
+              <div className="h-0.5 bg-fgTertiary w-full rounded-full mb-2.5" />
+              {files.map((file) =>
+                selectedAlbumViewMode === "list" ? (
+                  <ListViewDisplay
+                    key={file.name}
+                    file={file}
+                    onPlay={playAudio}
                   />
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-fgPrimary">
-                      {file.name.replace(".mp3", "")}
-                    </span>
-                    <span className="text-xs text-fgSecondary">
-                      {file.artist || "Unknown Artist"}
-                    </span>
-                  </div>
-                </div>
-                <div className="col-span-4">
-                  <span className="text-sm text-fgSecondary">
-                    {file.album || "Unknown Album"}
-                  </span>
-                </div>
-                <div>
-                  <span className="text-sm text-fgSecondary">
-                    {file.duration || "00:00"}
-                  </span>
-                </div>
-                <div className="place-items-center">
-                  <Heart className="size-5 text-fgSecondary cursor-pointer hover:text-accent" />
-                </div>
-              </div>
-            ) : (
-              <div
-                key={file.name}
-                className="p-2 grid grid-cols-12 items-center w-full bg-bgSecondary cursor-pointer hover:bg-fgTertiary rounded-lg"
-                onClick={() => playAudio(file)}>
-                <div className="col-span-4">
-                  <span className="text-sm text-fgPrimary font-semibold">
-                    {file.name.replace(".mp3", "")}
-                  </span>
-                </div>
-                <div className="col-span-3">
-                  <span className="text-sm text-fgSecondary">
-                    {file.artist || "Unknown Artist"}
-                  </span>
-                </div>
-                <div className="col-span-3">
-                  <span className="text-sm text-fgSecondary">
-                    {file.album || "Unknown Album"}
-                  </span>
-                </div>
-                <div>
-                  <span className="text-sm text-fgSecondary">
-                    {file.duration || "00:00"}
-                  </span>
-                </div>
-                <div className="place-items-center">
-                  <Heart className="size-4 text-fgSecondary cursor-pointer hover:text-accent" />
-                </div>
-              </div>
-            )
-          )}
+                ) : (
+                  <CompactViewDisplay
+                    key={file.name}
+                    file={file}
+                    onPlay={playAudio}
+                    albumDisplay={false}
+                  />
+                )
+              )}
+            </div>
+          </div>
+          <div className="sticky top-4 w-60 h-fit flex flex-col gap-4">
+            <img
+              src={files[0].albumArt}
+              alt={files[0].name}
+              className="min-w-60 max-w-60 h-60 rounded-lg"
+            />
+            <div className="flex gap-4">
+              <button className="rounded-full bg-accent px-4 py-1 flex items-center justify-center w-fit hover:bg-accent/80 transition-colors">
+                <Play
+                  className="size-5 text-bgSecondary"
+                  fill={"bg-bgSecondary"}
+                />
+                <span className="text-bgSecondary ml-2 text-xl font-bold">
+                  Play
+                </span>
+              </button>
+              <button className="rounded-full hover:bg-fgTertiary p-1">
+                <PlusCircle className="size-7 text-fgSecondary" />
+              </button>
+            </div>
+          </div>
         </div>
       </>
     );
