5824585e73bb62c02395f9e81c6112cc9a12fb33
clarence00 <clarencenatividad12@gmail.com>
2025-06-05 16:49:16 +0800

    list & compact view display components added

---
+++ app/components/CompactViewDisplay.tsx
@@ -0,0 +1,57 @@
+import { Heart } from "lucide-react";
+
+interface CompactViewSongDetails {
+  name: string;
+  album?: string;
+  artist?: string;
+  albumArt?: string;
+  duration?: string;
+}
+
+interface CompactViewDisplayProps {
+  file: CompactViewSongDetails;
+  onPlay: (file: CompactViewSongDetails) => void;
+  albumDisplay?: boolean;
+}
+
+const CompactViewDisplay = ({
+  file,
+  onPlay,
+  albumDisplay,
+}: CompactViewDisplayProps) => {
+  return (
+    <div
+      className="p-2 grid grid-cols-12 items-center w-full bg-bgPrimary cursor-pointer hover:bg-fgTertiary rounded-lg"
+      onClick={() => onPlay(file)}>
+      <div className={` ${albumDisplay ? "col-span-4" : "col-span-6"}`}>
+        <span className="text-sm text-fgPrimary font-semibold">
+          {file.name.replace(".mp3", "")}
+        </span>
+      </div>
+      {albumDisplay && (
+        <>
+          <div className="col-span-3">
+            <span className="text-sm text-fgSecondary">
+              {file.artist || "Unknown Artist"}
+            </span>
+          </div>
+          <div className="col-span-3">
+            <span className="text-sm text-fgSecondary">
+              {file.album || "Unknown Album"}
+            </span>
+          </div>
+        </>
+      )}
+      <div>
+        <span className="text-sm text-fgSecondary">
+          {file.duration || "00:00"}
+        </span>
+      </div>
+      <div className="place-items-center">
+        <Heart className="size-4 text-fgSecondary cursor-pointer hover:text-accent" />
+      </div>
+    </div>
+  );
+};
+
+export default CompactViewDisplay;
---
+++ app/components/ListViewDisplay.tsx
@@ -0,0 +1,65 @@
+import { Heart } from "lucide-react";
+
+interface ListViewSongDetails {
+  name: string;
+  album?: string;
+  artist?: string;
+  albumArt?: string;
+  duration?: string;
+}
+
+interface ListViewDisplayProps {
+  file: ListViewSongDetails;
+  onPlay: (file: ListViewSongDetails) => void;
+  albumDisplay?: boolean;
+}
+
+const ListViewDisplay = ({
+  file,
+  onPlay,
+  albumDisplay,
+}: ListViewDisplayProps) => {
+  const cleanedName = file.name.replace(".mp3", "");
+
+  return (
+    <div
+      className="p-2 grid grid-cols-12 items-center w-full bg-bgPrimary cursor-pointer hover:bg-fgTertiary rounded-lg"
+      onClick={() => onPlay(file)}>
+      <div
+        className={`flex gap-4 items-center ${
+          albumDisplay ? "col-span-5" : "col-span-6"
+        }`}>
+        <img
+          src={file.albumArt}
+          alt={file.name}
+          className="w-12 h-12 rounded-lg"
+        />
+        <div className="flex flex-col">
+          <span className="text-sm font-semibold text-fgPrimary">
+            {file.name.replace(".mp3", "")}
+          </span>
+          <span className="text-xs text-fgSecondary">
+            {file.artist || "Unknown Artist"}
+          </span>
+        </div>
+      </div>
+      {albumDisplay && (
+        <div className="col-span-4">
+          <span className="text-sm text-fgSecondary">
+            {file.album || "Unknown Album"}
+          </span>
+        </div>
+      )}
+      <div>
+        <span className="text-sm text-fgSecondary">
+          {file.duration || "00:00"}
+        </span>
+      </div>
+      <div className="place-items-center">
+        <Heart className="size-5 text-fgSecondary cursor-pointer hover:text-accent" />
+      </div>
+    </div>
+  );
+};
+
+export default ListViewDisplay;
