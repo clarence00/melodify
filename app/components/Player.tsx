54956ca75966d664a1be8c760c489dcf4c698663
clarence00 <clarencenatividad12@gmail.com>
2025-06-18 17:39:30 +0800

    player component frontend updated

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -1,6 +1,15 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Play, Pause, SkipBack, SkipForward, Volume2 } from "lucide-react";
+import {
+  Play,
+  Pause,
+  SkipBack,
+  SkipForward,
+  Volume2,
+  Heart,
+  Ellipsis,
+  PlusCircle,
+} from "lucide-react";
 import { useAudio } from "../context/AudioContext";
 
 const Player = () => {
@@ -71,48 +80,30 @@ const Player = () => {
   if (!currentFile) return null;
 
   return (
-    <div className="flex items-center justify-between px-4 h-full">
-      <div className="flex items-center gap-4 w-1/4">
-        {currentFile.albumArt && (
-          <img
-            src={currentFile.albumArt}
-            alt={currentFile.name}
-            className="w-12 h-12 rounded"
+    <div className="flex items-center justify-between px-6 h-full">
+      <div className="flex items-center gap-4 w-[40%]">
+        <button
+          onClick={togglePlay}
+          className="p-2 hover:text-fgPrimary text-bgPrimary rounded-full bg-accent hover:bg-accent/70">
+          {isPlaying ? (
+            <Pause className="size-5" />
+          ) : (
+            <Play className="size-5" />
+          )}
+        </button>
+        <button className="p-2 hover:bg-fgTertiary rounded-full text-fgPrimary">
+          <SkipBack
+            className="size-4 icon-filled"
+            onClick={playPrevious}
           />
-        )}
-        <div>
-          <p className="text-sm font-bold">
-            {currentFile.name.replace(".mp3", "")}
-          </p>
-          <p className="text-xs text-fgSecondary">{currentFile.artist}</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center w-1/2">
-        <div className="flex items-center gap-4">
-          <button className="p-2 hover:text-accent">
-            <SkipBack
-              className="size-4"
-              onClick={playPrevious}
-            />
-          </button>
-          <button
-            onClick={togglePlay}
-            className="p-2 hover:text-accent">
-            {isPlaying ? (
-              <Pause className="size-4" />
-            ) : (
-              <Play className="size-4" />
-            )}
-          </button>
-          <button className="p-2 hover:text-accent">
-            <SkipForward
-              className="size-4"
-              onClick={playNext}
-            />
-          </button>
-        </div>
-        <div className="flex items-center gap-2 w-full mt-2">
+        </button>
+        <button className="p-2 hover:bg-fgTertiary rounded-full text-fgPrimary">
+          <SkipForward
+            className="size-4 icon-filled"
+            onClick={playNext}
+          />
+        </button>
+        <div className="flex items-center gap-2 w-full">
           <span className="text-xs">{formatTime(currentTime)}</span>
           <div className="w-full h-1 bg-bgMain rounded-full">
             <div
@@ -123,19 +114,52 @@ const Player = () => {
           <span className="text-xs">{formatTime(duration)}</span>
         </div>
       </div>
-
-      <div className="w-1/4 flex items-center justify-end">
-        <Volume2 className="size-4 mr-2" />
-        <input
-          type="range"
-          min={0}
-          max={1}
-          step={0.01}
-          value={volume}
-          onChange={handleVolumeChange}
-          className="w-24 custom-range"
-          style={{ "--value": volume } as React.CSSProperties}
-        />
+      <div className="h-[65%] w-0.5 bg-fgSecondary/50 rounded-full" />
+      <div className="flex items-center justify-between w-[55%] h-full">
+        <div className="flex gap-2.5 justify-start items-center">
+          {currentFile.albumArt && (
+            <img
+              src={currentFile.albumArt}
+              alt={currentFile.name}
+              className="w-12 h-12 rounded-md"
+            />
+          )}
+          <div className="flex flex-col">
+            <span className="text-sm font-bold text-fgPrimary">
+              {currentFile.name.replace(".mp3", "")}
+            </span>
+            <span className="text-xs text-fgSecondary">
+              {currentFile.artist}
+            </span>
+            <span className="text-xs text-fgSecondary">
+              {currentFile.album}
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-1 text-fgSecondary items-center">
+          <button className="p-2 hover:bg-fgTertiary hover:text-accent rounded-full">
+            <Heart className="size-5" />
+          </button>
+          <button className="p-2 hover:bg-fgTertiary hover:text-accent rounded-full">
+            <PlusCircle className="size-5" />
+          </button>
+          <button className="p-2 hover:bg-fgTertiary hover:text-accent rounded-full">
+            <Ellipsis className="size-5" />
+          </button>
+          <div className=" flex ml-1.5 items-center justify-end">
+            <Volume2 className="size-4 mr-2" />
+            <input
+              type="range"
+              min={0}
+              max={1}
+              step={0.01}
+              value={volume}
+              onChange={handleVolumeChange}
+              className="w-20 custom-range"
+              style={{ "--value": volume } as React.CSSProperties}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
